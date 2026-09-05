614da58d26825af61ef5daadec8cc8951567ac0e
nicofdez0408 <nicofdez0408@gmail.com>
2026-09-05 13:26:13 -0400

    Implementación backend de operaciones CRUD de equipos

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,2 +1,3 @@
-from src.mascotas.schemas import Mascota
-from src.personas.schemas import Persona
+#from src.mascotas.schemas import Mascota
+#from src.personas.schemas import Persona
+from src.equipos.schemas import Equipo
