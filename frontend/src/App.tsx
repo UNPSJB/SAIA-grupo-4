68cff90a0cddc6eb1cef1b611c5d5aba88c4a851
nicofdez0408 <nicofdez0408@gmail.com>
2026-09-10 14:43:46 -0400

    Instalación y configuración base de React Router en App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,29 @@
 import { Box, ChakraProvider, defaultSystem } from '@chakra-ui/react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import EquiposPage from './pages/EquiposPage';
 
 export default function App() {
   return (
     <ChakraProvider value={defaultSystem}>
       <Box bg="gray.100" minH="100vh" w="100%">
-        <EquiposPage />
+        <BrowserRouter>
+          <Routes>
+            {/* Si se va a la raíz ("/"), redirige a "/equipos" por ahora */}
+            <Route path="/" element={<Navigate to="/equipos" replace />} />
+            
+            {/* Rutas activas */}
+            <Route path="/equipos" element={<EquiposPage />} />
+            
+            {/* Aca irian las otras rutas */}
+            
+            {/* Ruta por si se escribe una URL que no existe */}
+            <Route path="*" element={
+              <Box p={4} textAlign="center">
+                <h2>Página no encontrada</h2>
+              </Box>
+            } />
+          </Routes>
+        </BrowserRouter>
       </Box>
     </ChakraProvider>
   );
