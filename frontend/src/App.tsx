7c3a3a672e2b4f382db9eaec483ec1316acac597
santiago.czk <santiagoczk04@gmail.com>
2026-09-10 09:30:02 -0300

    React Route para Insumos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,20 @@
 import { Box, ChakraProvider, defaultSystem } from '@chakra-ui/react';
 import InsumoPage from './pages/InsumoPage';
+import { BrowserRouter } from 'react-router-dom'; 
+import { Routes, Route } from 'react-router-dom'
 
 export default function App() {
   return (
-    <ChakraProvider value={defaultSystem}>
-      <Box bg="gray.100" minH="100vh" w="100%">
-        <InsumoPage />
-      </Box>
-    </ChakraProvider>
+    <BrowserRouter>
+      <ChakraProvider value={defaultSystem}>
+        <Box bg="gray.100" minH="100vh" w="100%">
+
+          <Routes>
+            <Route path='/insumos' element={<InsumoPage/>}/>
+          </Routes>
+          
+        </Box>
+      </ChakraProvider>  
+    </BrowserRouter>
   );
 }
