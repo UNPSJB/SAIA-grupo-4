b68bac5973143ee89061f99ff3a1dc252a6f0176
Nicolas Muñoz <nicolaskapoim@gmail.com>
2026-09-20 22:32:01 -0300

    me olvide esa linea

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,5 +1,5 @@
 from src.equipos.schemas import Equipo
 from src.sectores.schemas import Sector
 from src.insumos.schemas import Insumo
-
+from src.insumo_quimico.schemas import InsumoQuimico
 from src.unidad_medida.schemas import UnidadMedida
